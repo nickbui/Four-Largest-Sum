26cec421963dd92683b1cd0812fd8c7f6cbbc6ce
nickbui <nicholas.t.bui@gmail.com>
2021-02-04 11:51:15 -0500

    Added comments and different cases. Added information to README

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Four-Largest-Sum
+
+This program takes an array and return the sum of the largest 4 numbers. If the array has less than 4 numbers, it will return -1. 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,30 +6,47 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        int arr[] = new int[8];
-        arr[0] = 20;
-        arr[1] = 3;
-        arr[2] = 500;
-        arr[3] = -60;
-        arr[4] = 1;
-        arr[5] = 10;
-        arr[6] = -40;
-        arr[7] = 50;
 
-        System.out.println(sum(arr));
+        // Case 4 number array
+        int arrFour[] = new int[4];
+        arrFour[0] = 1;
+        arrFour[1] = 1;
+        arrFour[2] = 1;
+        arrFour[3] = -5;
+        System.out.println("Case 1- 4 number array : Sum = " + sum(arrFour));
+
+        // Case more than 4 numbers in array
+        int arrBig[] = new int[8];
+        arrBig[0] = 20;
+        arrBig[1] = 3;
+        arrBig[2] = 500;
+        arrBig[3] = -60;
+        arrBig[4] = 1;
+        arrBig[5] = 10;
+        arrBig[6] = -40;
+        arrBig[7] = 50;
+        System.out.println("Case 2- More than 4 number array : Sum = " + sum(arrBig));
+
+        // Case less than 4 numbers in array
+        int arrSmall[] = new int[3];
+        arrSmall[0] = 4;
+        arrSmall[1] = 400;
+        arrSmall[2] = 500;
+        System.out.println("Case 3- Less than 4 number array : Sum = " + sum(arrSmall));
     }
 
     public static int sum(int arr[]) {
         int sumOfArr = 0;
+        //If the array only has 4 numbers
         if (arr.length == 4) {
             for (int i = 0; i < arr.length; i++) {
                 sumOfArr = sumOfArr + arr[i];
             }
+        //If the array has more than 4 numbers
         } else if (arr.length > 4) {
             Arrays.sort(arr);
             for (int i = arr.length - 1; i > arr.length - 5; i--) {
                 sumOfArr = sumOfArr + arr[i];
-                System.out.println(arr[i]);
             }
         } else {
             return -1;
